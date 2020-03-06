5d48f86a572967df083df1ff7b696582b35c477e
Danilo <methodic4u@gmail.com>
2020-03-06 11:09:29 -0300

    redux

--- src/components/CounterOutput/CounterOutput.js
+++ src/components/CounterOutput/CounterOutput.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 import './CounterOutput.css';
 
-const counterOutput = (props) => (
-    <div className="CounterOutput">
-        Current Counter: {props.value}
-    </div>
-);
-
+const counterOutput = (props) => {
+    return(        
+        <div className="CounterOutput">
+            Current Counter: {props.value}
+        </div>
+    );
+}
 export default counterOutput;
--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,41 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
+import * as actionTypes from '../../store/actions';
+
 class Counter extends Component {
     state = {
-        counter: 0
+        addAlgarism: 10,
+        subtractAlgarism: 8
     }
 
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
+    
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                {/* <CounterOutput value={this.state.counter} /> */}
+                <CounterOutput value={this.props.ctr} />
+                {/* <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} /> */}
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementeCounter}  />
+                <CounterControl label={"Add "+this.state.addAlgarism} clicked={() => this.props.add( this.state.addAlgarism )}  />
+                <CounterControl label={"Subtract "+this.state.subtractAlgarism} clicked={() => this.props.subtract( this.state.subtractAlgarism )}  />
+                <hr/>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store result</button>
+                <ul>
+                    {this.props.storedResults.map(res => (<li key={res.id} onClick={() => this.props.onDeleteResult(res.id)}>{res.value}</li>))}
+                    
+                </ul>
             </div>
         );
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementeCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        add: (value) => dispatch({type: actionTypes.ADD, value}),
+        subtract: (value) => dispatch({type: actionTypes.SUBTRACT, value}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, id})
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
